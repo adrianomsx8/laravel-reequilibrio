a9d4bbff69c056e323073bc9b89feb56c8d8a0b2
Adriano Souza <adriano.msx8@gmail.com>
2020-08-14 17:23:01 -0300

    ajustes imagem componente de card

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,7 +1,7 @@
 <template> 
 
     <div class="card col-sm-6 col-md-4" style="width: 18rem;">
-        <img class="card-img-top" v-bind:src="imagem" alt="">
+        <img class="img-thumbnail img-fluid rounded" v-bind:src="imagem" v-bind:alt="nome">
         <div class="card-body" >
             <h5 class="card-title">{{nome}}</h5>
             <p class="card-text">{{descricao}}</p>
@@ -15,3 +15,12 @@
       props: ['titulo', 'nome', 'descricao', 'imagem', 'detalhar']
     }
 </script>
+<style >
+.container img {
+    max-width:200px;
+    max-height:150px;
+    width: 200px;
+    height: 150px;
+     object-fit: fill;
+}
+</style>
